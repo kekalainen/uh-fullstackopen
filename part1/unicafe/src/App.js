19b8341acc624db344ac77768f7963195def56c8
Kekalainen <git@kekalainen.me>
2022-04-23 05:14:28 +0300

    1.6: Implement feedback & statistics

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,12 +1,28 @@
 import { useState } from 'react';
 
+const Button = ({ label, onClick }) => (
+  <button onClick={onClick}>{label}</button>
+);
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button label="good" onClick={() => setGood(good + 1)} />
+      <Button label="neutral" onClick={() => setNeutral(neutral + 1)} />
+      <Button label="bad" onClick={() => setBad(bad + 1)} />
+      <h1>Statistics</h1>
+      <ul>
+        <li>good: {good}</li>
+        <li>neutral: {neutral}</li>
+        <li>bad: {bad}</li>
+      </ul>
+    </div>
+  );
 };
 
 export default App;
