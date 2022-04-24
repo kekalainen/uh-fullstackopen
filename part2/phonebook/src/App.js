db740a0ad4e77b90de745935d2ea1a0f76cb8df7
Kekalainen <git@kekalainen.me>
2022-04-24 20:14:02 +0300

    2.11: Fetch data using axios

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,10 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 import { Filter, PersonForm, Persons } from './components';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
@@ -30,6 +26,10 @@ const App = () => {
   };
 
   const handleNameFilterChange = (event) => setNameFilter(event.target.value);
+
+  useEffect(() => {
+    axios.get('//localhost:3001/persons').then((res) => setPersons(res.data));
+  }, []);
 
   return (
     <div>
