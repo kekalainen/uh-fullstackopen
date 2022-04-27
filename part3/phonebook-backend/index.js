2a727728ed75aa56c6394d11894988afb3c86fd5
Kekalainen <git@kekalainen.me>
2022-04-27 07:17:30 +0300

    3.18: Remove hardcoded data

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -7,29 +7,6 @@ const port = process.env.PORT || 3001;
 
 const Person = require('./models/person');
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
 const errorHandler = (error, _request, response, next) => {
   if (error.name === 'CastError')
     return response.status(400).send({ error: 'malformed id' });
