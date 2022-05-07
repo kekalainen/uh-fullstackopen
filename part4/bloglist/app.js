50cb137b572b4ad8a4dfa2bef551038eacd9a553
Kekalainen <git@kekalainen.me>
2022-05-07 09:42:07 +0300

    4.2: Extract endpoints to separate module

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,4 +1,4 @@
-const Blog = require('./models/blog');
+const blogsRouter = require('./controllers/blogs');
 const config = require('./utils/config');
 const cors = require('cors');
 const express = require('express');
@@ -11,18 +11,6 @@ mongoose.connect(config.MONGODB_URI);
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use('/api/blogs', blogsRouter);
 
 module.exports = app;
