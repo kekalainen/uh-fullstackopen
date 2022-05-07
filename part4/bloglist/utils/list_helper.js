dd201f6ac778a1fd8e2694cbe7aff60a8e7f5573
Kekalainen <git@kekalainen.me>
2022-05-07 12:35:34 +0300

    4.5: Add favorite blog util function & tests

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,8 +1,14 @@
 const dummy = (_blogs) => 1;
 
+const favoriteBlog = (blogs) =>
+  blogs.reduce((favorite, current) =>
+    current.likes > favorite.likes ? current : favorite
+  );
+
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
 module.exports = {
   dummy,
+  favoriteBlog,
   totalLikes,
 };
