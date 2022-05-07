52794ad92c2c0be8b170adabf5384a59e5d417dc
Kekalainen <git@kekalainen.me>
2022-05-07 09:00:44 +0300

    4.1: Set up npm package

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const http = require('http');
 const express = require('express');
 const app = express();
@@ -13,8 +14,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = 'mongodb://localhost/bloglist';
-mongoose.connect(mongoUrl);
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
