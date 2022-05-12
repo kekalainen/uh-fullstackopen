97330f6b62fbf3d91e8988f22f64145b477049e6
Kekalainen <git@kekalainen.me>
2022-05-12 15:44:39 +0300

    4.16: Add tests for creating user w/ invalid data

--- part4/bloglist/tests/user_api.test.js
+++ part4/bloglist/tests/user_api.test.js
@@ -8,6 +8,8 @@ const User = require('../models/user');
 
 const api = supertest(app);
 
+const userPayload = { username: 'test', name: 'Test', password: 'test' };
+
 beforeEach(async () => {
   await User.deleteMany({});
 
@@ -36,6 +38,34 @@ describe('creating a user', () => {
 
     expect(await User.countDocuments()).toEqual(initialCount + 1);
     expect(await User.exists({ username: newUser.username })).not.toBeNull();
+  });
+
+  test('fails with missing properties', () =>
+    api.post('/api/users').send({}).expect(400));
+
+  test('fails with a username shorter than 3 characters', () =>
+    api
+      .post('/api/users')
+      .send({ ...userPayload, username: 'te' })
+      .expect(400));
+
+  test('fails with a password shorter than 3 characters', () =>
+    api
+      .post('/api/users')
+      .send({ ...userPayload, password: 'te' })
+      .expect(400));
+
+  test('fails with an existing username', async () => {
+    const initialCount = await User.countDocuments();
+
+    const response = await api
+      .post('/api/users')
+      .send({ ...userPayload, username: 'azurediamond' })
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body.error).toContain('username must be unique');
+    expect(initialCount).toEqual(await User.countDocuments());
   });
 });
 
