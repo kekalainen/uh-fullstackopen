854169aa3be01485a1b3474edc3ecb0725f0c6c4
Kekalainen <git@kekalainen.me>
2022-05-12 05:19:25 +0300

    4.12: Add error handler middleware

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ require('express-async-errors');
 
 const blogsRouter = require('./controllers/blogs');
 const config = require('./utils/config');
+const middleware = require('./utils/middleware');
 
 const app = express();
 
@@ -14,5 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.errorHandler);
 
 module.exports = app;
