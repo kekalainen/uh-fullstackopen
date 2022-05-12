19c8133411318224c2733f7e676abcf97797e949
Kekalainen <git@kekalainen.me>
2022-05-12 04:44:21 +0300

    4.10: Refactor blog creation to use async/await

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -8,12 +8,10 @@ router.get('/', async (_request, response) => {
   response.json(blogs);
 });
 
-router.post('/', (request, response) => {
+router.post('/', async (request, response) => {
   const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  const result = await blog.save();
+  response.status(201).json(result);
 });
 
 module.exports = router;
