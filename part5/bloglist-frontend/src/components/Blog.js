ce8e51ecbad88ea6a011d5c8454a65eb95758930
Kekalainen <git@kekalainen.me>
2022-05-14 05:08:36 +0300

    5.7: Implement expanding/collapsing blogs

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div>
+      <div>
+        {blog.title} {blog.author}{' '}
+        <button onClick={() => setExpanded(!expanded)}>
+          {expanded ? 'collapse' : 'expand'}
+        </button>
+      </div>
+      {expanded && (
+        <>
+          <a target="_blank" rel="noreferrer" href={blog.url}>
+            {blog.url}
+          </a>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.user.name}</div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
