8c74c744a6824db8f52592e4d9ec17a98425424a
Kekalainen <git@kekalainen.me>
2022-05-16 08:04:08 +0300

    5.17: Add DB reset route

--- part5/bloglist-backend/app.js
+++ part5/bloglist-backend/app.js
@@ -17,6 +17,11 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.authTokenExtractor);
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing', testingRouter);
+}
+
 app.use('/api/auth', authRouter);
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', userRouter);
