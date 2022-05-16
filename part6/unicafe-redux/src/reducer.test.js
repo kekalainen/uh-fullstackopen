4726c43789e669b4e1f3696afe0ccb00305aa1d8
Kekalainen <git@kekalainen.me>
2022-05-16 23:51:12 +0300

    6.1: Test formatting & styling

--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -2,34 +2,25 @@ import deepFreeze from 'deep-freeze';
 import counterReducer from './reducer';
 
 describe('unicafe reducer', () => {
-  const initialState = {
+  const zeroState = {
     good: 0,
     ok: 0,
     bad: 0,
   };
 
-  test('should return a proper initial state when called with undefined state', () => {
-    const state = {};
-    const action = {
-      type: 'DO_NOTHING',
-    };
+  it('should return a proper initial state when called with undefined state', () => {
+    const action = { type: 'DO_NOTHING' };
 
-    const newState = counterReducer(undefined, action);
-    expect(newState).toEqual(initialState);
+    const state = counterReducer(undefined, action);
+    expect(state).toEqual(zeroState);
   });
 
-  test('good is incremented', () => {
-    const action = {
-      type: 'GOOD',
-    };
-    const state = initialState;
+  it('handles the GOOD action', () => {
+    const action = { type: 'GOOD' };
+    const initialState = zeroState;
 
-    deepFreeze(state);
-    const newState = counterReducer(state, action);
-    expect(newState).toEqual({
-      good: 1,
-      ok: 0,
-      bad: 0,
-    });
+    deepFreeze(initialState);
+    const state = counterReducer(initialState, action);
+    expect(state).toEqual({ ...zeroState, good: 1 });
   });
 });
