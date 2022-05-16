f457c3223bbc6af3e389d730f8d2bf6e45fa0017
Kekalainen <git@kekalainen.me>
2022-05-16 08:42:29 +0300

    5.18: Add tests for logging in

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,11 +1,36 @@
+const user = {
+  name: 'Tester',
+  username: 'tester',
+  password: 'hunter2',
+};
+
+const attemptLogin = ({ username, password }) => {
+  cy.get('input[name="username"]').type(username);
+  cy.get('input[name="password"]').type(password);
+  cy.get('button[type="submit"]').click();
+};
+
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    cy.request('POST', 'http://localhost:3000/api/users', user);
     cy.visit('http://localhost:3000');
   });
 
   it('shows the login form', function () {
     cy.contains(/log in/i);
     cy.get('form');
+  });
+
+  describe('Login', function () {
+    it('succeeds with valid credentials', function () {
+      attemptLogin(user);
+      cy.contains(/log out/i);
+    });
+
+    it('fails with invalid credentials', function () {
+      attemptLogin({ username: 'invalid', password: 'invalid' });
+      cy.get('.notification.error').contains(/invalid/i);
+    });
   });
 });
