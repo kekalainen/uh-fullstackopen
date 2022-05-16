1c9190897e6366970eb3d163712b4e4bf2c89c04
Kekalainen <git@kekalainen.me>
2022-05-16 23:53:12 +0300

    6.1: Add tests for remaining actions

--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -23,4 +23,31 @@ describe('unicafe reducer', () => {
     const state = counterReducer(initialState, action);
     expect(state).toEqual({ ...zeroState, good: 1 });
   });
+
+  it('handles the OK action', () => {
+    const action = { type: 'OK' };
+    const initialState = zeroState;
+
+    deepFreeze(initialState);
+    const state = counterReducer(initialState, action);
+    expect(state).toEqual({ ...zeroState, ok: 1 });
+  });
+
+  it('handles the BAD action', () => {
+    const action = { type: 'BAD' };
+    const initialState = zeroState;
+
+    deepFreeze(initialState);
+    const state = counterReducer(initialState, action);
+    expect(state).toEqual({ ...zeroState, bad: 1 });
+  });
+
+  it('handles the ZERO action', () => {
+    const action = { type: 'ZERO' };
+    const initialState = { good: 6, ok: 2, bad: 5 };
+
+    deepFreeze(initialState);
+    const state = counterReducer(initialState, action);
+    expect(state).toEqual(zeroState);
+  });
 });
