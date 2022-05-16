c7bff85b1319014db0c66613f7ec89d5cde30d95
Kekalainen <git@kekalainen.me>
2022-05-16 22:57:42 +0300

    6.1: Formatting

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -1,23 +1,23 @@
 const initialState = {
   good: 0,
   ok: 0,
-  bad: 0
-}
+  bad: 0,
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
+  console.log(action);
   switch (action.type) {
     case 'GOOD':
-      return state
+      return state;
     case 'OK':
-      return state
+      return state;
     case 'BAD':
-      return state
+      return state;
     case 'ZERO':
-      return state
-    default: return state
+      return state;
+    default:
+      return state;
   }
-  
-}
+};
 
-export default counterReducer
+export default counterReducer;
