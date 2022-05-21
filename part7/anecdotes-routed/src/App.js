5b9405df0602aee861f8cd40be4851a897b860e6
Kekalainen <git@kekalainen.me>
2022-05-21 21:22:01 +0300

    7.2: Implement single anecdote view

--- part7/anecdotes-routed/src/App.js
+++ part7/anecdotes-routed/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useMatch } from 'react-router-dom';
 import About from './components/About';
+import Anecdote from './components/Anecdote';
 import AnecdoteList from './components/AnecdoteList';
 import CreateNew from './components/CreateNew';
 import Footer from './components/Footer';
@@ -26,6 +27,9 @@ const App = () => {
 
   const [notification, setNotification] = useState('');
 
+  const match = useMatch('/anecdotes/:id'),
+    anecdote = match ? anecdotes.find((a) => a.id === +match.params.id) : null;
+
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000);
     setAnecdotes(anecdotes.concat(anecdote));
@@ -50,6 +54,10 @@ const App = () => {
       <Menu />
       <Routes>
         <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route
+          path="/anecdotes/:id"
+          element={<Anecdote anecdote={anecdote} />}
+        />
         <Route path="/create" element={<CreateNew addNew={addNew} />} />
         <Route path="/about" element={<About />} />
       </Routes>
