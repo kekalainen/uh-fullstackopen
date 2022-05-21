317fded147d65d54c3835fb0d5de19eac946b9e9
Kekalainen <git@kekalainen.me>
2022-05-22 01:01:34 +0300

    7.7: Implement useCountry hook

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -18,7 +18,14 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null);
 
-  useEffect(() => {});
+  useEffect(() => {
+    if (name)
+      axios
+        .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+        .then((response) => setCountry(response.data[0]))
+        .catch(() => setCountry(null));
+    else setCountry(null);
+  }, [name]);
 
   return country;
 };
