27ff7638ffe886ec7ec0b7a752db8907a0e50b92
Kekalainen <git@kekalainen.me>
2022-09-19 06:06:47 +0300

    8.11: Implement editing authors

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,12 +1,27 @@
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
+import { useState } from 'react';
+import { EDIT_AUTHOR } from '../graphql/mutations';
 import { GET_ALL_AUTHORS } from '../graphql/queries';
 
 const Authors = (props) => {
   const { data, loading } = useQuery(GET_ALL_AUTHORS);
 
+  const [name, setName] = useState('');
+  const [year, setYear] = useState('');
+  const [editAuthor] = useMutation(EDIT_AUTHOR);
+
   if (!props.show || loading) return null;
 
   const authors = data.allAuthors;
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    editAuthor({ variables: { name, setBornTo: +year } });
+
+    setName('');
+    setYear('');
+  };
 
   return (
     <div>
@@ -27,6 +42,27 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <h3>set birthyear</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          ></input>
+        </label>
+        <br />
+        <label>
+          year
+          <input
+            type="number"
+            value={year}
+            onChange={({ target }) => setYear(target.value)}
+          ></input>
+        </label>
+        <br />
+        <button type="submit">update author</button>
+      </form>
     </div>
   );
 };
--- part8/library-frontend/src/graphql/mutations.js
+++ part8/library-frontend/src/graphql/mutations.js
@@ -1,5 +1,14 @@
 import { gql } from '@apollo/client';
 
+export const EDIT_AUTHOR = gql`
+  mutation EditAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      born
+      id
+    }
+  }
+`;
+
 export const ADD_BOOK = gql`
   mutation AddBook(
     $author: String!
