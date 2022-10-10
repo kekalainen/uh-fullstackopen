1bab6825363512a7b17d2c6a0ecaec4e69dc5d79
Kekalainen <git@kekalainen.me>
2022-10-10 22:47:31 +0300

    8.18: Include auth token in requests

--- part8/library-frontend/src/graphql/client.js
+++ part8/library-frontend/src/graphql/client.js
@@ -1,10 +1,26 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const AUTH_TOKEN_LOCAL_STORAGE_KEY = 'library-auth-token';
+
+const authLink = setContext((_operation, { headers }) => {
+  const authToken = localStorage.getItem(AUTH_TOKEN_LOCAL_STORAGE_KEY);
+
+  return {
+    headers: {
+      ...headers,
+      ...(authToken ? { authorization: `bearer ${authToken}` } : {}),
+    },
+  };
+});
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000',
+});
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'http://localhost:4000',
-  }),
+  link: authLink.concat(httpLink),
 });
 
 export default client;
