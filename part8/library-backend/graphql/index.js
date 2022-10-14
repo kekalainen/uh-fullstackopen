6600b9cdb099501b38635cdf185ceb38431078ed
Kekalainen <git@kekalainen.me>
2022-10-15 00:20:22 +0300

    8.23: Extract GraphQL context into a module

--- part8/library-backend/graphql/index.js
+++ part8/library-backend/graphql/index.js
@@ -1,7 +1,9 @@
+const context = require('./context');
 const resolvers = require('./resolvers');
 const typeDefs = require('./types');
 
 module.exports = {
+  context,
   resolvers,
   typeDefs,
 };
--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,23 +1,14 @@
 const { ApolloServer } = require('apollo-server');
-const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
-const { resolvers, typeDefs } = require('./graphql');
-const { JWT_SECRET, MONGODB_URI } = require('./utils/config');
-const User = require('./models/user');
+const { context, resolvers, typeDefs } = require('./graphql');
+const { MONGODB_URI } = require('./utils/config');
 
 mongoose.connect(MONGODB_URI);
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: async ({ req }) => {
-    const auth = req?.headers?.authorization;
-    if (auth && auth.toLowerCase().startsWith('bearer ')) {
-      const { id } = jwt.verify(auth.substring(7), JWT_SECRET);
-      const authUser = await User.findById(id);
-      return { authUser };
-    }
-  },
+  context,
 });
 
 server.listen().then(({ url }) => {
