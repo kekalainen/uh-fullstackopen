1a4552586654518042890162403ca003bdd9b3f0
Kekalainen <git@kekalainen.me>
2022-10-15 02:54:08 +0300

    8.24: Set up graphql-ws for subscriptions

--- part8/library-frontend/src/graphql/client.js
+++ part8/library-frontend/src/graphql/client.js
@@ -1,7 +1,12 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { Kind, OperationTypeNode } from 'graphql';
+import { createClient } from 'graphql-ws';
 
 const AUTH_TOKEN_LOCAL_STORAGE_KEY = 'library-auth-token';
+const PRURL = '//localhost:4000/';
 
 const authLink = setContext((_operation, { headers }) => {
   const authToken = localStorage.getItem(AUTH_TOKEN_LOCAL_STORAGE_KEY);
@@ -15,12 +20,30 @@ const authLink = setContext((_operation, { headers }) => {
 });
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000',
+  uri: `http:${PRURL}`,
 });
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: `ws:${PRURL}`,
+  })
+);
+
+const splitLink = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return (
+      kind === Kind.OPERATION_DEFINITION &&
+      operation === OperationTypeNode.SUBSCRIPTION
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
+  link: splitLink,
 });
 
 export default client;
