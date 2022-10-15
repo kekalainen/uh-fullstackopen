4a332bd544a8a1a8fd213069c770df2a4d075dce
Kekalainen <git@kekalainen.me>
2022-10-15 06:37:43 +0300

    8.25: Use fragment for bookAdded selections

--- part8/library-frontend/src/graphql/subscriptions.js
+++ part8/library-frontend/src/graphql/subscriptions.js
@@ -1,18 +1,12 @@
 import { gql } from '@apollo/client';
+import { BOOK_DETAILS } from './fragments';
 
 export const BOOK_ADDED = gql`
   subscription BookAdded {
     bookAdded {
-      author {
-        bookCount
-        born
-        id
-        name
-      }
-      genres
-      id
-      published
-      title
+      ...BookDetails
     }
   }
+
+  ${BOOK_DETAILS}
 `;
