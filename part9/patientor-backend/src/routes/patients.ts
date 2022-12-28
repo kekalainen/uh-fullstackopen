943bcb7e41d371ed726e6614ccc1201aa448b89b
Kekalainen <git@kekalainen.me>
2022-12-28 05:58:31 +0200

    9.12: Implement patient creation endpoint

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,10 +1,25 @@
-import express from 'express';
+import express, { Request } from 'express';
 import patientService from '../services/patientService';
+import { NewPatient } from '../types';
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
   res.send(patientService.getNonSensitive());
+});
+
+router.post('/', (req: Request<unknown, unknown, NewPatient>, res) => {
+  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+
+  const patient = patientService.add({
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation,
+  });
+
+  res.json(patient);
 });
 
 export default router;
--- part9/patientor-backend/src/services/patientService.ts
+++ part9/patientor-backend/src/services/patientService.ts
@@ -1,6 +1,18 @@
 import data from '../../data/patients.json';
 
-import { NonSensitivePatient, Patient } from '../types';
+import { v4 as uuid } from 'uuid';
+import { NewPatient, NonSensitivePatient, Patient } from '../types';
+
+const add = (payload: NewPatient): Patient => {
+  const patient: Patient = {
+    id: uuid(),
+    ...payload,
+  };
+
+  data.push(patient);
+
+  return patient;
+};
 
 const getAll = (): Patient[] => {
   return data;
@@ -17,6 +29,7 @@ const getNonSensitive = (): NonSensitivePatient[] => {
 };
 
 export default {
+  add,
   getAll,
   getNonSensitive,
 };
