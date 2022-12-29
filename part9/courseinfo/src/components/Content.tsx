21d3573e92ec9472bd83ac497bec3efc3000732a
Kekalainen <git@kekalainen.me>
2022-12-29 03:58:54 +0200

    9.14: Extract components

---
+++ part9/courseinfo/src/components/Content.tsx
@@ -0,0 +1,17 @@
+import { CoursePart } from '../types';
+
+const Content = ({ courseParts }: { courseParts: CoursePart[] }) => (
+  <>
+    <p>
+      {courseParts[0].name} {courseParts[0].exerciseCount}
+    </p>
+    <p>
+      {courseParts[1].name} {courseParts[1].exerciseCount}
+    </p>
+    <p>
+      {courseParts[2].name} {courseParts[2].exerciseCount}
+    </p>
+  </>
+);
+
+export default Content;
