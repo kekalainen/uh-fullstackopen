965ce7f7ce734ebe03cfb7ed924c42ed3ccdef7c
Kekalainen <git@kekalainen.me>
2022-12-29 04:08:59 +0200

    9.15: Update data & types

--- part9/courseinfo/src/types.ts
+++ part9/courseinfo/src/types.ts
@@ -1,4 +1,26 @@
-export interface CoursePart {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
+  type: string;
 }
+
+interface CourseNormalPart extends CoursePartBase {
+  type: 'normal';
+  description: string;
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: 'groupProject';
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartBase {
+  type: 'submission';
+  description: string;
+  exerciseSubmissionLink: string;
+}
+
+export type CoursePart =
+  | CourseNormalPart
+  | CourseProjectPart
+  | CourseSubmissionPart;
